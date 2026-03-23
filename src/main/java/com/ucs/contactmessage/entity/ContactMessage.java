b63f475a86276d5a4801835c83ee71ecfe6a2f79
Ümit Can Sariboga <ucsariboga@gmail.com>
2026-03-23 13:31:52 +0100

    feat: initialize school management system with Spring Boot

---
+++ src/main/java/com/ucs/contactmessage/entity/ContactMessage.java
@@ -0,0 +1,44 @@
+package com.ucs.contactmessage.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "t_contact_message")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ContactMessage {
+
+    @Id
+    @GeneratedValue(generator = "sequence",
+            strategy = GenerationType.SEQUENCE)
+        @SequenceGenerator(name = "squence",
+        sequenceName = "guest_seq",
+        initialValue = 1000,
+        allocationSize = 10)
+    @Column(name = "id",updatable = false)
+    private Long id;
+
+    @Column(nullable = false,length = 50)
+    private String name;
+
+    @Column(nullable = false,length = 50)
+    private String email;
+
+    @Column(nullable = false,length = 50)
+    private String subject;
+
+    @Column(nullable = false,length = 1000)
+    private String message;
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime dateTime;
+
+}
